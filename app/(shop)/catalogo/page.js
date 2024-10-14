7c9310a999427166eddd281768d566f63b9b8199
mishainx <fer.r@live.com.ar>
2024-10-14 20:44:38 -0300

    modificaciones

--- app/(shop)/catalogo/page.js
+++ app/(shop)/catalogo/page.js
@@ -1,16 +1,6 @@
 import CatalogueContainer from "@/components/catalogue/CatalogueContainer";
 
 export default async function Catalogo() {
-  // Fetch de productos
-  const productsResponse = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products`);
-  const productsData = await productsResponse.json();
-  const products = productsData.payload;
-
-  // Fetch de categorías
-  const categoriesResponse = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/categories`);
-  const categoriesData = await categoriesResponse.json();
-  const categories = categoriesData.payload;
-
   return (
     <main className="flex flex-col items-center justify-start min-h-screen py-10">
       {/* Título de la página */}
@@ -20,9 +10,6 @@ export default async function Catalogo() {
           <div className="absolute inset-x-0 -bottom-2 mx-auto w-full h-1 bg-red-500"></div>
         </h1>
       </div>
-
-      {/* Pasar productos y categorías al componente */}
-      <CatalogueContainer products={products} categories={categories} />
     </main>
   );
 }
