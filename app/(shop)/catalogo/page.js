c11958ace40e1a2c78bb15d65b6312c9e4131cd5
mishainx <fer.r@live.com.ar>
2024-10-14 20:52:11 -0300

    modificaciones

--- app/(shop)/catalogo/page.js
+++ app/(shop)/catalogo/page.js
@@ -1,6 +1,16 @@
 import CatalogueContainer from "@/components/catalogue/CatalogueContainer";
 
 export default async function Catalogo() {
+  
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products`,{cache:"no-store",next:{revalidate:3600}}); // Ajusta la URL según sea necesario
+  const data = await response.json()
+  const products = data.payload
+
+  // Fetch de categorías
+  const categoriesResponse = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/categories`);
+  const categoriesData = await categoriesResponse.json();
+  const categories = categoriesData.payload;
+
   return (
     <main className="flex flex-col items-center justify-start min-h-screen py-10">
       {/* Título de la página */}
@@ -10,6 +20,9 @@ export default async function Catalogo() {
           <div className="absolute inset-x-0 -bottom-2 mx-auto w-full h-1 bg-red-500"></div>
         </h1>
       </div>
+
+      {/* Pasar productos y categorías al componente */}
+      <CatalogueContainer products={products} categories={categories} />
     </main>
   );
 }
--- src/components/product/productList.js
+++ src/components/product/productList.js
@@ -5,7 +5,6 @@ export default async function ProductList() {
   const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products`,{cache:"no-store",next:{revalidate:3600}}); // Ajusta la URL según sea necesario
   const data = await response.json()
   const products = data.payload
-  console.log(products)
 
     return (
       <div className="w-full p-5 gap-4 flex flex-wrap justify-center">
