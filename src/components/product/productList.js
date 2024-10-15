b3aa7759d3f24f3fc29ac888094471b5c3636600
mishainx <fer.r@live.com.ar>
2024-10-15 15:56:01 -0300

    modificaciones

--- src/components/product/productList.js
+++ src/components/product/productList.js
@@ -7,10 +7,6 @@ export default async function ProductList() {
   const data = await response.json()
   const products = data.payload
 
-  const responseCategories = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/categories`,{next:{revalidate:3600}}); // Ajusta la URL según sea necesario
-  const dataCategories = await responseCategories.json()
-  const categories = dataCategories.payload
-
     return (
       <div className="w-full p-5 gap-4 flex flex-wrap justify-center">
           <CatalogueContainer products={products} categories={categories}/>              
