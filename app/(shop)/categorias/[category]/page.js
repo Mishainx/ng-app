8c5bd8e9d49dbfa623ca5209c1f290f9dde699dc
mishainx <fer.r@live.com.ar>
2024-10-14 22:01:10 -0300

    modificaciones

--- app/(shop)/categorias/[category]/page.js
+++ app/(shop)/categorias/[category]/page.js
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import ProductList from '@/components/product/productList';
 
 export default function Categories({ params }) {
@@ -6,7 +5,6 @@ export default function Categories({ params }) {
 
   return (
     <main>
-
       <section>
         <ProductList />
       </section>
