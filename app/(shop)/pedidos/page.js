51477609e525564f8770f5d6ffdadfed15054d44
mishainx <fer.r@live.com.ar>
2024-10-15 00:00:09 -0300

    modificaciones

--- app/(shop)/pedidos/page.js
+++ app/(shop)/pedidos/page.js
@@ -2,15 +2,13 @@
 export default async function Pedidos() {
   async function getCategories() {
     try {
-      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products`); // Ajusta la URL según sea necesario
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/categories`); // Ajusta la URL según sea necesario
       
       if (!response.ok) {
         throw new Error(`Error: ${response.status} ${response.statusText}`);
       }
   
-      const data = await response.json();
-      console.log(data.payload[0])
-  
+      const data = await response.json();  
       const categories = data.payload;
       return categories;
     } catch (error) {
@@ -20,8 +18,30 @@ export default async function Pedidos() {
     }
   }
 
-  await getCategories()
+  async function getProducts() {
+    try {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products`); // Ajusta la URL según sea necesario
+      
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status} ${response.statusText}`);
+      }
+  
+      const data = await response.json();
+      console.log(data.payload[0])
+  
+      const products = data.payload;
+      return products;
+    } catch (error) {
+      console.log("el error es aqui")
+      console.error("Error fetching categories:", error);
+      return null; // O ajusta esto según cómo quieras manejar el error en tu aplicación
+    }
+  }
+  
 
+
+  const categories = await getCategories()
+  const products = await getProducts()
     return (
       <main>
           <div className="h-screen">
