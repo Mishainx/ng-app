48565578bcb5209d095acf710229726e7fdad280
mishainx <fer.r@live.com.ar>
2024-10-14 21:47:23 -0300

    modificaciones

--- app/(shop)/catalogo/page.js
+++ app/(shop)/catalogo/page.js
@@ -1,6 +1,24 @@
 import CatalogueData from "@/components/catalogue/CatalogueData";
 
-export default function Catalogo() {
+export default async function Catalogo() {
+
+  try {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products`, {next:{revalidate:3600}});
+    if (!response.ok) {
+      throw new Error('Error fetching product');
+    }
+    const data = await response.json();
+    products = data.payload;
+
+  } catch (error) {
+    console.error("Error fetching product:", error);
+    return (
+      <main className="p-4">
+        <h1 className="text-2xl font-bold">Error al cargar el producto</h1>
+        <p className="text-red-500">No se pudo obtener la información del producto. Inténtalo más tarde.</p>
+      </main>
+    );
+  }
   
   return (
     <main className="flex flex-col items-center justify-start min-h-screen py-10">
--- app/(shop)/ofertas/page.js
+++ app/(shop)/ofertas/page.js
@@ -1,9 +1,5 @@
 
 export default async function Ofertas() {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products`,{cache:"no-store",next:{revalidate:3600}}); // Ajusta la URL según sea necesario
-  const data = await response.json()
-  const products = data.payload
-  console.log(products[0])
 
     return (
       <main>
