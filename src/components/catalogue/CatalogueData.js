4912f22243dc3ca601da5e066338ad18f9c0a31e
mishainx <fer.r@live.com.ar>
2024-10-14 21:25:49 -0300

    modificaciones

--- src/components/catalogue/CatalogueData.js
+++ src/components/catalogue/CatalogueData.js
@@ -2,7 +2,7 @@ import CatalogueContainer from "@/components/catalogue/CatalogueContainer";
 
 export default async function CatalogueData() {
 
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products`,next:{revalidate:3600}); // Ajusta la URL según sea necesario
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products`,{next:{revalidate:3600}}); // Ajusta la URL según sea necesario
     const data = await response.json()
     const products = data.payload
   
