6888cb474c91a7fb0b48b0ebb4b12a50267d6d54
mishainx <fer.r@live.com.ar>
2024-10-15 15:54:03 -0300

    modificaciones

--- src/components/product/productList.js
+++ src/components/product/productList.js
@@ -1,3 +1,4 @@
+import CatalogueContainer from '../catalogue/CatalogueContainer';
 import ProductCard from './productCard';
 
 export default async function ProductList() {
@@ -6,8 +7,14 @@ export default async function ProductList() {
   const data = await response.json()
   const products = data.payload
 
+  const responseCategories = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/categories`,{next:{revalidate:3600}}); // Ajusta la URL según sea necesario
+  const dataCategories = await responseCategories.json()
+  const categories = dataCategories.payload
+
     return (
       <div className="w-full p-5 gap-4 flex flex-wrap justify-center">
+          <CatalogueContainer products={products} categories={categories}/>              
+          
                         {products.map(product => (
           <ProductCard key={product.id} product={product} />
         ))}
