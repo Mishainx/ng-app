4d4afbdfae36e6384bc7d1251204625c0b161c14
mishainx <fer.r@live.com.ar>
2024-10-14 21:58:44 -0300

    modificaciones

--- app/(shop)/catalogo/page.js
+++ app/(shop)/catalogo/page.js
@@ -1,25 +1,5 @@
-import CatalogueData from "@/components/catalogue/CatalogueData";
-
 export default async function Catalogo() {
 
-  try {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products`, {next:{revalidate:3600}});
-    if (!response.ok) {
-      throw new Error('Error fetching product');
-    }
-    const data = await response.json();
-    const products = data.payload;
-
-  } catch (error) {
-    console.error("Error fetching product:", error);
-    return (
-      <main className="p-4">
-        <h1 className="text-2xl font-bold">Error al cargar el producto</h1>
-        <p className="text-red-500">No se pudo obtener la información del producto. Inténtalo más tarde.</p>
-      </main>
-    );
-  }
-  
   return (
     <main className="flex flex-col items-center justify-start min-h-screen py-10">
       {/* Título de la página */}
@@ -29,7 +9,6 @@ export default async function Catalogo() {
           <div className="absolute inset-x-0 -bottom-2 mx-auto w-full h-1 bg-red-500"></div>
         </h1>
       </div>
-    <CatalogueData/>
     </main>
   );
 }
--- src/components/product/productList.js
+++ src/components/product/productList.js
@@ -2,7 +2,7 @@ import ProductCard from './productCard';
 
 export default async function ProductList() {
 
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products`,{cache:"no-store",next:{revalidate:3600}}); // Ajusta la URL según sea necesario
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products`,{next:{revalidate:3600}}); // Ajusta la URL según sea necesario
   const data = await response.json()
   const products = data.payload
 
