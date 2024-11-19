052527d5c62308f2d20f610666f0e321520c55d8
mishainx <fer.r@live.com.ar>
2024-11-19 20:56:23 -0300

    modificaciones

--- src/components/product/productList.js
+++ src/components/product/productList.js
@@ -4,6 +4,7 @@ export default async function ProductList() {
   const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products`); // Ajusta la URL según sea necesario
   const data = await response.json();
   const products = data.payload;
+  console.log(data.total)
   return (
     <div className="w-full flex flex-wrap justify-center">
       <CatalogueContainer products={products} total={data.total} /> {/* Añadido el total para paginación */}
