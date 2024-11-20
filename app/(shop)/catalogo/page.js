79bec5ad068ca59e8fd2b23cb47e942b25c0c807
mishainx <fer.r@live.com.ar>
2024-11-19 22:27:29 -0300

    modificaciones

--- app/(shop)/catalogo/page.js
+++ app/(shop)/catalogo/page.js
@@ -1,25 +1,6 @@
 import ProductList from "@/components/product/productList";
 
 export default async function Catalogo() {
-  async function getProducts() {
-    try {
-      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products`); // Ajusta la URL según sea necesario
-      
-      if (!response.ok) {
-        throw new Error(`Error: ${response.status} ${response.statusText}`);
-      }
-  
-      const data = await response.json();
-  
-      const products = data.payload;
-      return products;
-    } catch (error) {
-
-      console.error("Error fetching products:", error);
-      return null; // O ajusta esto según cómo quieras manejar el error en tu aplicación
-    }
-  }
-  const products = await getProducts()
 
   return (
     <main className="w-full flex flex-col items-center justify-start min-h-screen py-10">
@@ -30,7 +11,7 @@ export default async function Catalogo() {
           <div className="absolute inset-x-0 -bottom-2 mx-auto w-full h-1 bg-red-500"></div>
         </h1>
       </div>
-      <ProductList products={products} />
+      <ProductList />
     </main>
   );
 }
--- src/components/product/productList.js
+++ src/components/product/productList.js
@@ -1,6 +1,26 @@
 import CatalogueContainer from '../catalogue/CatalogueContainer';
 
-export default async function ProductList({products}) {
+export default async function ProductList() {
+  async function getProducts() {
+    try {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products`); // Ajusta la URL según sea necesario
+      
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status} ${response.statusText}`);
+      }
+  
+      const data = await response.json();
+  
+      const products = data.payload;
+      return products;
+    } catch (error) {
+
+      console.error("Error fetching products:", error);
+      return null; // O ajusta esto según cómo quieras manejar el error en tu aplicación
+    }
+  }
+  const products = await getProducts()
+
   return (
     <div className="w-full flex flex-wrap justify-center">
       <CatalogueContainer products={products} /> {/* Añadido el total para paginación */}
