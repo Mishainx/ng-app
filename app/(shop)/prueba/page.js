2ce3b6c3ac6dc7b8f79ade7d8ff3137fdeea7ffc
mishainx <fer.r@live.com.ar>
2024-11-19 22:07:15 -0300

    modificaciones

--- app/(shop)/prueba/page.js
+++ app/(shop)/prueba/page.js
@@ -1,7 +1,21 @@
 export default async function Prueba() {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products`); // Ajusta la URL según sea necesario
-  const data = await response.json();
-  const products = data.payload;
+  let products = [];
+  let total = 0;
+
+  try {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products`); // Ajusta la URL según sea necesario
+    
+    if (!response.ok) {
+      throw new Error(`Error fetching products: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    products = data.payload;
+    total = data.total;
+  } catch (error) {
+    console.error('Error fetching products:', error);
+    // Puedes agregar un manejo de error más específico si lo deseas, como un mensaje de fallback
+  }
   return (
     <div className="w-full flex flex-wrap justify-center">
       <p>{data.total}</p>
