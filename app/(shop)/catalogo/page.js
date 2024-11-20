a4695314bb3f8b45a4c8510af273367015a14ff6
mishainx <fer.r@live.com.ar>
2024-11-19 22:19:44 -0300

    modificaciones

--- app/(shop)/catalogo/page.js
+++ app/(shop)/catalogo/page.js
@@ -1,10 +1,19 @@
 import ProductList from "@/components/product/productList";
 
 export default async function Catalogo() {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products`); // Ajusta la URL según sea necesario
-  const data = await response.json();
-  const products = data.payload;
-  console.log(products)
+  const getProducts = async () => {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products`, {
+      next: { revalidate: 60 } // Configura la revalidación si es necesario
+    });
+    if (!response.ok) {
+      throw new Error('Failed to fetch products');
+    }
+    const data = await response.json();
+    return data.payload || [];
+  };
+
+  const products = await getProducts();
+  console.log(products);
 
   return (
     <main className="w-full flex flex-col items-center justify-start min-h-screen py-10">
@@ -15,7 +24,7 @@ export default async function Catalogo() {
           <div className="absolute inset-x-0 -bottom-2 mx-auto w-full h-1 bg-red-500"></div>
         </h1>
       </div>
-      <ProductList products={products}/>
+      <ProductList products={products} />
     </main>
   );
 }
