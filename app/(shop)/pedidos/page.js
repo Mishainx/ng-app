47fd12c4b53b04e4b49b9bf63d5367455a3b5879
mishainx <fer.r@live.com.ar>
2024-11-19 22:59:57 -0300

    modificaciones

--- app/(shop)/pedidos/page.js
+++ app/(shop)/pedidos/page.js
@@ -1,26 +1,6 @@
 
 export default async function Pedidos() {
 
-  async function getProducts() {
-    try {
-      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products`); // Ajusta la URL según sea necesario
-      
-      if (!response.ok) {
-        throw new Error(`Error: ${response.status} ${response.statusText}`);
-      }
-  
-      const data = await response.json();
-  
-      const products = data.payload;
-      return products;
-    } catch (error) {
-
-      console.error("Error fetching products:", error);
-      return null; // O ajusta esto según cómo quieras manejar el error en tu aplicación
-    }
-  }
-  const products = await getProducts()
-
     return (
       <main>
           <div className="h-screen">
