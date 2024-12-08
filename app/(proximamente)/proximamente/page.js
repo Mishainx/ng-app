99d7c5e1307535130eee3c477fbf55cb8522c9f2
mishainx <fer.r@live.com.ar>
2024-12-08 11:43:10 -0300

    modfiicaciones

--- app/(proximamente)/proximamente/page.js
+++ app/(proximamente)/proximamente/page.js
@@ -11,17 +11,17 @@ export default function Proximamente() {
       <div className="absolute inset-0 bg-gradient-radial from-[#FF3B3F] via-[#00091B] to-transparent opacity-60"></div>
 
       {/* Contenedor principal */}
-      <div className="relative text-center max-w-3xl z-10 flex flex-col items-center justify-center">
+      <div className="relative text-center max-w-3xl z-10 flex flex-col items-center justify-center space-y-10">
         
         {/* Título */}
-        <h1 className="text-4xl sm:text-4xl md:text-4xl font-bold leading-tight mb-6 relative top-12">
+        <h1 className="text-4xl sm:text-4xl md:text-5xl font-bold leading-tight mb-6">
           Próximamente<span className="text-red-500">.</span>
         </h1>
 
-        {/* En pantallas grandes (LG) y superiores, Sonic y Mario deben separarse */}
-        <div className="flex flex-col lg:flex-row justify-center lg:gap-20 mb-10 items-center relative top-20 ">
+        {/* Contenedor de imágenes Sonic y Mario */}
+        <div className="flex flex-col lg:flex-row justify-center gap-20 items-center">
           {/* Sonic */}
-          <div className="hidden lg:block w-1/6 drop-shadow-2xl relative top-10">
+          <div className="hidden lg:block w-1/6 drop-shadow-2xl">
             <Image
               src="/proximamente/sonic-nippongame.png"
               alt="Sonic Nippon Game"
@@ -32,7 +32,7 @@ export default function Proximamente() {
           </div>
 
           {/* Logo centrado */}
-          <div className="w-full sm:w-2/3 md:w-1/2 lg:w-2/3 mx-auto mb-6 flex justify-center">
+          <div className="w-full sm:w-2/3 md:w-1/2 lg:w-2/3 mx-auto flex justify-center">
             <Image
               src="/nippon-game-logo.png"
               alt="Logo Nippon Game"
@@ -43,7 +43,7 @@ export default function Proximamente() {
           </div>
 
           {/* Mario */}
-          <div className="hidden relative lg:block w-1/6 drop-shadow-2xl top-10">
+          <div className="hidden lg:block w-1/6 drop-shadow-2xl">
             <Image
               src="/proximamente/mario-nippongame.png"
               alt="Mario Nippon Game"
@@ -55,17 +55,17 @@ export default function Proximamente() {
         </div>
 
         {/* Subtítulo */}
-        <p className="text-base font-bold sm:text-lg md:text-xl mb-10 relative top-10">
+        <p className="text-base font-bold sm:text-lg md:text-xl mb-10">
           Estamos trabajando para traerte algo increíble.
         </p>
 
         {/* Mensaje de contacto */}
-        <p className="text-base  font-bold sm:text-lg md:text-xl mb-10 relative top-10">
+        <p className="text-base font-bold sm:text-lg md:text-xl mb-10">
           Mientras tanto, contáctanos en:
         </p>
 
         {/* Iconos de contacto */}
-        <div className="flex justify-center space-x-8 mb-10 relative top-10">
+        <div className="flex justify-center space-x-8 mb-10">
           <div className="cursor-pointer p-5 text-white rounded-full bg-red-500/90 hover:bg-red-800 transition duration-200 ease-in-out flex items-center justify-center hover:shadow-2xl hover:scale-105">
             <Link href="https://maps.app.goo.gl/cCr4VrpNMuDqTppJA">
               <MapsIcon className="w-10 h-10" />
