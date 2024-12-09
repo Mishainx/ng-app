a736ba5fce2d51d5546c9b0ac70820c280a7043b
mishainx <fer.r@live.com.ar>
2024-12-09 11:46:21 -0300

    open grahp

--- app/(proximamente)/layout.js
+++ app/(proximamente)/layout.js
@@ -2,8 +2,21 @@ import "../globals.css";
 
 
 export const metadata = {
-  title: 'Próximamente',
-  description: 'Próximamente NipponGame expertos en videojuegos',
+  title: 'Nippongame - Próximamente',
+  description: 'Ecommerce mayorista experto en videojuegos',
+  openGraph: {
+    title: 'Nippongame - Próximamente',
+    description: 'Ecommerce mayorista experto en videojuegos',
+    url: 'https://www.nippongame.com.ar',
+    images: [
+      {
+        url: '/proximamente/nippon-game-logo.png',  // Ruta de la imagen
+        width: 1200,  // Ancho de la imagen
+        height: 630,  // Alto de la imagen
+        alt: 'Nippongame logo',  // Descripción alternativa de la imagen
+      },
+    ],
+  }
 }
 
 export default function RootLayout({ children }) {
--- app/(shop)/layout.js
+++ app/(shop)/layout.js
@@ -16,6 +16,19 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Nippon Game",
   description: "Ecommerce mayorista de videojuegos",
+  openGraph: {
+    title: 'Nippongame - Próximamente',
+    description: 'Ecommerce mayorista experto en videojuegos',
+    url: 'https://www.nippongame.com.ar',
+    images: [
+      {
+        url: '/proximamente/nippon-game-logo.png',  // Ruta de la imagen
+        width: 1200,  // Ancho de la imagen
+        height: 630,  // Alto de la imagen
+        alt: 'Nippongame logo',  // Descripción alternativa de la imagen
+      },
+    ],
+  }
 };
 
 export default function RootLayout({ children }) {
