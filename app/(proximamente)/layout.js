80bf5e80f9e298f20bc66d7d508a3854b9fc2079
mishainx <fer.r@live.com.ar>
2024-12-19 12:08:51 -0300

    modificaciones

--- app/(proximamente)/layout.js
+++ app/(proximamente)/layout.js
@@ -10,7 +10,7 @@ export const metadata = {
     url: 'https://www.nippongame.com.ar',
     images: [
       {
-        url: '/proximamente/nippon-game-logo.png',  // Ruta de la imagen
+        url: 'https://www.nippongame.com.ar/proximamente/nippon-game-logo.png',  // Ruta de la imagen
         width: 1200,  // Ancho de la imagen
         height: 630,  // Alto de la imagen
         alt: 'Nippongame logo',  // Descripción alternativa de la imagen
--- app/(shop)/page.js
+++ app/(shop)/page.js
@@ -13,7 +13,7 @@ export const metadata = {
     url: 'https://www.nippongame.com.ar',
     images: [
       {
-        url: '/proximamente/nippon-game-logo.png',  // Ruta de la imagen
+        url: 'https://www.nippongame.com.ar/proximamente/nippon-game-logo.png',  // Ruta de la imagen
         width: 1200,  // Ancho de la imagen
         height: 630,  // Alto de la imagen
         alt: 'Nippongame logo',  // Descripción alternativa de la imagen
