bcc932fee077951a0dce24fa5822bc9b7b4d140d
mishainx <fer.r@live.com.ar>
2024-12-23 01:25:41 -0300

    modificaciones

--- app/(shop)/page.js
+++ app/(shop)/page.js
@@ -27,7 +27,7 @@ export default async function Home() {
 
   try {
     const response = await fetch(
-      `${process.env.NEXT_PUBLIC_API_URL}/api/products/featured`, { next:{revalidate:3600} }
+      `${process.env.NEXT_PUBLIC_API_URL}/api/products/featured`, { next:{revalidate:0} }
     );
 
     const products = await response.json();
