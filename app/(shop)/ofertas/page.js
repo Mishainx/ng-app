818844c29233ad3a7db82d6749dd95fd05a5b83f
mishainx <fer.r@live.com.ar>
2024-12-23 01:38:35 -0300

    modificaciones

--- app/(shop)/ofertas/page.js
+++ app/(shop)/ofertas/page.js
@@ -22,7 +22,7 @@ export default async function Ofertas() {
     <main>
       <div className="h-full">
         {/* Banner responsivo */}
-        <div className="relative w-full h-48 bg-cover bg-center overflow-hidden mb-6" style={{ backgroundImage: 'url("/ruta/a/tu/banner.jpg")' }}>
+        <div className="relative w-full h-48 bg-cover bg-center overflow-hidden mb-6" >
           <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50">
             <h2 className="text-3xl font-bold text-white text-center">¡Ofertas Especiales!</h2>
           </div>
