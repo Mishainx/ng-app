b443ed8b39fd287d3a789945f3ce503510d33dc3
mishainx <fer.r@live.com.ar>
2024-12-23 03:12:25 -0300

    modificaciones

--- app/(shop)/ofertas/page.js
+++ app/(shop)/ofertas/page.js
@@ -19,7 +19,7 @@ export default async function Ofertas() {
         </div>
 
         {/* Componente de productos en oferta */}
-        <OfferProducts offersProducts={offersProducts} />
+    <OffersProducts offersProducts={offersProducts} /> 
       </div>
     </main>
   );
