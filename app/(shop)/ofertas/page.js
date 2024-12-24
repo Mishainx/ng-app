64c68956858bd1d18d7b8ba655f793c84efdef06
mishainx <fer.r@live.com.ar>
2024-12-23 22:27:06 -0300

    modificaciones

--- app/(shop)/ofertas/page.js
+++ app/(shop)/ofertas/page.js
@@ -2,6 +2,7 @@
 
 import OffersProducts from "@/components/offers/offers";
 import { useProducts } from "@/context/ProductsContext";
+import CustomHero from "@/components/catalogue/CustomHero";
 
 export default function Ofertas() {
   const { products, error } = useProducts(); // Obtener los productos desde el contexto
@@ -18,11 +19,7 @@ export default function Ofertas() {
     <main>
       <div className="h-full">
         {/* Banner responsivo */}
-        <div className="relative w-full h-48 bg-cover bg-center overflow-hidden mb-6">
-          <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50">
-            <h2 className="text-3xl font-bold text-white text-center">¡Ofertas Especiales!</h2>
-          </div>
-        </div>
+            <CustomHero title={"Ofertas!"} description={"Los mejores descuentos en nuestros productos"} img={"/catalogue/catalogue-img-nippongame.png"}/>
 
         {/* Componente de productos en oferta */}
         <OffersProducts offersProducts={offersProducts} />
