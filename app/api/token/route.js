0246237e1abc78bcfb4eec5c8e1985a98ace5a00
mishainx <fer.r@live.com.ar>
2025-02-14 19:55:38 -0300

    modificaciones

--- app/api/token/route.js
+++ app/api/token/route.js
@@ -3,10 +3,14 @@ import { authAdmin } from '@/firebase/authManager';
 
 export async function POST(req) {
   try {
+    console.log('Request received');
+
     // Obtener el token del cuerpo de la solicitud
     const { token } = await req.json();
+    console.log('Token received:', token);
 
     if (!token) {
+      console.log('No token provided');
       return NextResponse.json(
         { message: 'Token not provided' },
         { status: 400 }
@@ -16,9 +20,12 @@ export async function POST(req) {
     // Verificar el token con Firebase Admin SDK
     let decodedToken;
     try {
+      console.log('Verifying token with Firebase Admin SDK...');
       decodedToken = await authAdmin.verifyIdToken(token);
+      console.log('Token verified:', decodedToken);
     } catch (error) {
-      console.log(error);
+      console.error('Error verifying token:', error.message);
+
       // El token es inválido, eliminar la cookie
       const response = NextResponse.json(
         { message: 'Invalid token' },
@@ -35,11 +42,13 @@ export async function POST(req) {
     }
 
     // Devolver la información del usuario decodificado
+    console.log('Token is valid, returning response...');
     return NextResponse.json(
       { message: 'Token is valid', user: decodedToken },
       { status: 200 }
     );
   } catch (error) {
+    console.error('Error processing request:', error.message);
     return NextResponse.json(
       { message: 'Error processing request', error: error.message },
       { status: 500 }
