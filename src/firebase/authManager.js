a7b766f5c2dc1b13b8ff28ac6affb5c899bfab34
mishainx <fer.r@live.com.ar>
2025-02-14 23:19:43 -0300

    modificaciones

--- src/firebase/authManager.js
+++ src/firebase/authManager.js
@@ -1,10 +1,13 @@
-import admin from "firebase-admin"
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
+import admin from "firebase-admin";
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 if (!admin.apps.length) {
     admin.initializeApp({
       credential: admin.credential.cert(serviceAccount),
     });
-  }
+}
 
-export const authAdmin= admin.auth();
+// Verificar si la inicialización fue exitosa
+console.log("Firebase Admin SDK inicializado correctamente");
+
+export const authAdmin = admin.auth();
