0775a78877a1f1706866046856cfaa2f200babdf
mishainx <fer.r@live.com.ar>
2025-05-05 22:25:58 -0300

    modifcaciones

--- app/(shop)/Carrousel/page.js
+++ app/(shop)/Carrousel/page.js
@@ -1,7 +1,8 @@
 import HeroCarousel from "@/components/Carrousel/Carrousel";
+import { cache } from "react";
 
 export default async function CarrouselPage() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/slides`, { next: { revalidate: 100 } });
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/slides`, {cache: "no-cache"});
   const data = await res.json();
   const visibleSlides = data.payload?.filter((s) => s.visible).sort((a, b) => a.order - b.order) || [];
   return (
