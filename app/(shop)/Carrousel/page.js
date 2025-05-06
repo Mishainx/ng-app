c9075b93e9ade331b825d9849bf8e2957b3455c2
mishainx <fer.r@live.com.ar>
2025-05-05 22:22:08 -0300

    modificaciones

--- app/(shop)/Carrousel/page.js
+++ app/(shop)/Carrousel/page.js
@@ -1,10 +1,9 @@
 import HeroCarousel from "@/components/Carrousel/Carrousel";
 
 export default async function CarrouselPage() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/slides`, { cache: 'no-cache' });
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/slides`, { next: { revalidate: 60 } });
   const data = await res.json();
   const visibleSlides = data.payload?.filter((s) => s.visible).sort((a, b) => a.order - b.order) || [];
-
   return (
     <main>
       <HeroCarousel slides={visibleSlides} />
