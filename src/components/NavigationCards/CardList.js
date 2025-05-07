3bfd7cd28902500a61192ef1a29e2345938424f5
mishainx <fer.r@live.com.ar>
2025-05-07 19:53:02 -0300

    modificaciones

--- src/components/NavigationCards/CardList.js
+++ src/components/NavigationCards/CardList.js
@@ -62,7 +62,7 @@ export default function CardList({ cards }) {
   flushRow(); // cualquier sobrante
 
   return (
-    <div className="w-full min-h-screen flex flex-col items-center gap-6 p-10 max-w-7xl mx-auto">
+    <div className="w-full min-h-screen flex flex-col items-center gap-6 p-5 md:p-10 max-w-7xl mx-auto">
       {items}
     </div>
   );
